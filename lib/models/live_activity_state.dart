cf85b84f27fadac890157bce7721d913ac3c87c4
Dimitri Dessus <d.dessus@apparence.io>
2022-11-09 15:30:43 +0100

    🎨 Format files

--- lib/models/live_activity_state.dart
+++ lib/models/live_activity_state.dart
@@ -1,9 +1,11 @@
 enum LiveActivityState {
   /// The Live Activity is active, visible to the user, and can receive content updates
   active,
+
   /// The Live Activity is visible, but the user, app, or system ended it, and it won't update its content
   /// anymore.
   ended,
+
   /// The Live Activity ended and is no longer visible because the user or the system removed it.
   dismissed,
   unknown,
