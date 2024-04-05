34732327c061e49b91ead8d88b8113feba093470
Radwa Alaa <eng_radwa_alaa@yahoo.com>
2024-04-05 10:19:56 +0400

    update image from url min width to 90.

--- lib/models/live_activity_image.dart
+++ lib/models/live_activity_image.dart
@@ -1,6 +1,6 @@
 abstract class LiveActivityImage {
   int minHeight = 50;
-  int minWidth = 90;
+  int minWidth = 50;
 
   LiveActivityImage(this.minHeight, this.minWidth);
 }
@@ -14,7 +14,7 @@ class LiveActivityImageFromAsset extends LiveActivityImage {
   LiveActivityImageFromAsset(
     this.path, {
     int minHeight = 50,
-    int minWidth = 90,
+    int minWidth = 50,
   }) : super(minHeight, minWidth);
 }
 
@@ -27,6 +27,6 @@ class LiveActivityImageFromUrl extends LiveActivityImage {
   LiveActivityImageFromUrl(
     this.url, {
     int minHeight = 50,
-    int minWidth = 50,
+    int minWidth = 90,
   }) : super(minHeight, minWidth);
 }
